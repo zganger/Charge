ba655d1f0124a3fe3b4c58c105fab6b272405fd1
Zack Ganger <zganger@bu.edu>
2016-10-25 18:38:44 -0400

    reroute to https

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ var bodyParser      = require('body-parser');
 var methodOverride  = require('method-override');
 var app             = express();
 
+
+app.use (function (req, res, next) {
+    if (req.secure) {
+        // request was via https, so do no special handling
+        next();
+    } else {
+        // request was via http, so redirect to https
+        res.redirect('https://' + req.headers.host + req.url);
+    }
+});
+
 // Express Configuration
 // -----------------------------------------------------
 // Sets the connection to MongoDB
